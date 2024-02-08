c16dee4eb4858787aac782541d9f8186b0ee7798
Khanh Thanh <100078354+khanhthanhdev@users.noreply.github.com>
2024-02-08 13:51:46 +0000

    add option price, done landing page

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -1,7 +1,9 @@
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { pricingCards } from "@/lib/constants";
 import clsx from "clsx";
+import { Check } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -16,7 +18,7 @@ export default function Home() {
             Rapid
           </h1>
         </div>
-        <div className="flex justify-center items-center relative md:mt-[-70px]">
+        <div className="flex justify-center items-center relative md:mt-[-70px] mt-[-40px]">
           <Image src={'/assets/preview.png'} alt="banner image" height={1200} width={1200} className="rounded-tl-2xl rounded-tr-2xl border-2 border-muted" />
         </div>
         <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10">
@@ -46,6 +48,27 @@ export default function Home() {
                   {card.description}
                 </CardDescription>
               </CardHeader>
+              <CardContent>
+                <span className="text-4xl font-bold">{card.price}</span>
+                <span className="text-muted-foreground">/m</span>
+              </CardContent>
+              <CardFooter className="flex flex-col items-start gap-4">
+                <div>
+                  {card.features.map((feature) => (
+                    <div key={feature} className="flex gap-2 items-center">
+                        <Check className="text-muted-foreground" />
+                        <p>{feature}</p>
+                    </div>
+                  ))}
+                </div>
+                <Link href={`/agency?plan=${card.priceId}`}
+                className={clsx('w-full text-center bg-primary p-2 rounded-md', 
+                {'!bg-muted-foreground': card.title!== 'Unlimited Saas'}
+                )}
+                >
+                  Get Started
+                </Link>
+              </CardFooter>
             </Card>
         ))}
       </div>
