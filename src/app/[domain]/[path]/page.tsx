bf83583b46833485e9e2f5080f6b350e78c5eb43
Khanh Thanh <100078354+khanhthanhdev@users.noreply.github.com>
2024-02-13 15:51:53 +0000

    setup subdomain

---
+++ src/app/[domain]/[path]/page.tsx
@@ -0,0 +1,12 @@
+import { ClerkProvider } from '@clerk/nextjs'
+import React from 'react'
+
+const page = () => {
+  return (
+    <ClerkProvider>
+        
+    </ClerkProvider>
+  )
+}
+
+export default page
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
-import {ClerkProvider} from "@clerk/nextjs"
-import { dark} from "@clerk/themes"
+
 import { ThemeProvider } from "@/providers/theme-provider";
 
 const font = DM_Sans({ subsets: ["latin"] });
@@ -18,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider appearance={{baseTheme: dark}}>
+
       <html lang="en" suppressHydrationWarning>
       <body className={font.className}>
           <ThemeProvider attribute="class"
@@ -29,7 +28,7 @@ export default function RootLayout({
           </ThemeProvider>
       </body>
       </html>
-    </ClerkProvider>
+
 
   );
 }
